664b9c9253bcf08a4e997f001eefb531d592ea9c
lika <user75@hotmail.fr>
2025-02-21 22:34:54 +0100

    added function to get the print report of the count of characters, not sorted yet + readme update

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     count_characters = get_count_characters(text)
-    #print(f"{num_words} words found in the document")
+    liste_report = report_sort(count_characters) 
+    # print(f"{num_words} words found in the document")
     #print(count_characters)
+    print(liste_report)
 def get_num_words(text):
     words = text.split()
     return len(words)
@@ -23,8 +25,14 @@ def get_count_characters(text):
             characters_dict[i] = 1
     return characters_dict
         
-
-
+def report_sort(characters_dict):
+    liste =[]
+    for k,v in characters_dict.items():
+        if k.isalpha():
+            liste.append({"character": k , "value" : v})
+            print(f"The character {k} was found {v} times")
+    liste = sorted(liste, key = lambda x : x["value"], reverse=True )
+    
 
 main()
 
