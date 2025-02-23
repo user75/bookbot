7be4136f70af10fd338dd5a8a50539bd0fec548b
lika <user75@hotmail.fr>
2025-02-23 12:04:31 +0100

    added arguments with sys.argv to specify book path as input

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+import sys
 from stats import get_num_words, get_count_characters, report_sort
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
+    path = sys.argv[1]
+    text = get_book_text(path)
     num_words = get_num_words(text)
     count_characters = get_count_characters(text)
     print(f"Found {num_words} total words")
